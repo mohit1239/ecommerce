702df97140e566eafb9b6e94682d6c04e1cb8410
mohit1239 <msehrawat110@gmail.com>
2020-12-13 12:57:57 +0530

    fresh-commit

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,6 +5,7 @@ import {useStateValue} from"./StateProvider"
 import CheckoutProduct from "./CheckoutProduct"
 function Checkout() {
     const[{basket},dispatch]=useStateValue()
+
     return (
 
         <div className="checkout">
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,7 +3,6 @@ import "./Subtotal.css"
 import CurrencyFormat from "react-currency-format"
 import {useStateValue} from"./StateProvider"
 import {getBasketTotal} from "./reducer"
-import {Link} from "react-router-dom"
 import {useHistory} from "react-router-dom"
 function Subtotal() {
     const history=useHistory();
